3f94a5d71656c0117854f583b1d2acd93caf303e
nikx12 <nikhitazulka@gmail.com>
2018-11-01 17:59:47 +0530

    Added actions in redux

---
+++ redux/actions/action.js
@@ -0,0 +1,23 @@
+const ADD_TODO = 'TODO/TODO/ADD_TODO';
+const UPDATE_TODO = 'TODO/TODO/UPDATE_TODO';
+const DELETE_TODO = 'TODO/TODO/DELETE_TODO';
+
+export function addTodo(todo) {    
+    return {
+        type: ADD_TODO,
+        todo,
+    };
+}
+
+export function updateTodo(todo){
+    return {
+        type: UPDATE_TODO,        
+        todo,
+    }
+}
+
+export function deleteTodo(todo){
+    return {
+        type: DELETE_TODO,
+        todo,        
+    }
--- redux/reducers/reducer.js
+++ redux/reducers/reducer.js
@@ -1,3 +1,5 @@
+import {addTodo, updateTodo, deleteTodo} from '../actions/action'
+
 const ADD_TODO = 'TODO/TODO/ADD_TODO';
 const UPDATE_TODO = 'TODO/TODO/UPDATE_TODO';
 const DELETE_TODO = 'TODO/TODO/DELETE_TODO';
